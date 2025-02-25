23b415b8c155d1b1ad76f5d28d03aa149a56bd06
Dyusha12 <tamgdeya17@gmail.com>
2025-02-25 04:02:29 +0300

    Реализация генерирующего списка была перенесена в отдельную функцию, добавлена проверка на отрицательное число

--- FirstIndividualPythonApplication/FirstIndividualPythonApplication.py
+++ FirstIndividualPythonApplication/FirstIndividualPythonApplication.py
@@ -1,13 +1,18 @@
 import random
 
-id_list = 19
+def random_generate_list(number_elements):
+    if(number_elements > 0):
+        begin_range = 5 #Начало диапазона
+        end_range = number_elements * 100 #Конец диапазона
 
-begin_range = 5 #Начало диапазона
-end_range = id_list * 100 #Конец диапазона
-number_elements = id_list + 10 #Количество элементов
+        #Генерация элементов из диапазона
+        random_list = [random.randint(begin_range, end_range) for i in range(number_elements)]
+        return random_list
+    else:
+        return "Incorrect value"
 
-#Генерация элементов из диапазона
-random_list = [random.randint(begin_range, end_range) for i in range(number_elements)]
+id_list = -19 + 10
+random_numbers = random_generate_list(id_list)
 print("Generation process...")
 print("Randomly generated list: ")
-print(random_list)
+print(random_numbers)
