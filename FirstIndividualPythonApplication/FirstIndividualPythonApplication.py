b65a9854c79d9031c7943a88d4292551ed7accdf
Dyusha12 <tamgdeya17@gmail.com>
2025-02-25 03:53:07 +0300

    Реализован генерирующий список элементов случайным образом, по следующему принципу: из диапазона от 5 до № по журналу * 100 (число элементов № по журналу + 10)

--- FirstIndividualPythonApplication/FirstIndividualPythonApplication.py
+++ FirstIndividualPythonApplication/FirstIndividualPythonApplication.py
@@ -1,1 +1,13 @@
+import random
 
+id_list = 19
+
+begin_range = 5 #Начало диапазона
+end_range = id_list * 100 #Конец диапазона
+number_elements = id_list + 10 #Количество элементов
+
+#Генерация элементов из диапазона
+random_list = [random.randint(begin_range, end_range) for i in range(number_elements)]
+print("Generation process...")
+print("Randomly generated list: ")
+print(random_list)
